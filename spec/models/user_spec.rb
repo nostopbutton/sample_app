af67b55b10a0da66f5be8b9eb14028cd6e19cc67
Peter Stephens <peter.m.stephens@gmail.com>
2012-09-26 07:40:44 +0800

    Added user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -86,6 +86,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email is stored in downcase" do
+    let(:mixed_case_email) { "EmAiL@EmAiL.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
