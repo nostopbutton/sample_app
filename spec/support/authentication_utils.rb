c55e44b1a63f671d5055983a2d086f32c2bf60a7
Peter Stephens <peter.m.stephens@gmail.com>
2012-10-08 20:57:00 +0800

    Finish suer edit, update, index, and destroy action

--- spec/support/authentication_utils.rb
+++ spec/support/authentication_utils.rb
@@ -4,9 +4,20 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :be_signed_out do
   match do |page|
+    page.should_not have_link('Users')
     page.should_not have_link('Profile')
+    page.should_not have_link('Settings')
     page.should_not have_link('Sign out')
     page.should have_link('Sign in', href: signin_path)
   end
@@ -14,7 +25,9 @@ end
 
 RSpec::Matchers.define :be_signed_in do |user|
   match do |page|
-    page.should have_link('Profile', href: user_path(user))
+    page.should have_link('Users',    href: users_path)
+    page.should have_link('Profile',  href: user_path(user))
+    page.should have_link('Settings', href: edit_user_path(user))
     page.should_not have_link('Sign in')
     page.should have_link('Sign out', href: signout_path)
   end
--- spec/support/user_utils.rb
+++ spec/support/user_utils.rb
@@ -4,3 +4,11 @@ RSpec::Matchers.define :be_profile_page do |user|
     page.should have_selector('title', text: user.name)
   end
 end
+
+RSpec::Matchers.define :be_edit_page do #|user|
+  match do |page|
+    page.should have_selector('h1',     text: "Update your profile") 
+    page.should have_selector('title',  text: "Edit user")
+    page.should have_link('change',     href: 'http://gravatar.com/emails')
+  end
+end
